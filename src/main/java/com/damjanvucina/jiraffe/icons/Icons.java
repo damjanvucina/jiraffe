cc39228ab127181cd9937b67c5260df092129a7b
Damjan Vucina <vucinadamjan@gmail.com>
2020-04-11 01:47:37 +0200

    Add jiraffe svg

---
+++ src/main/java/com/damjanvucina/jiraffe/icons/Icons.java
@@ -0,0 +1,9 @@
+package com.damjanvucina.jiraffe.icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class Icons {
+    public static final Icon jiraffeIcon = IconLoader.getIcon("/icons/jiraffe-logo.svg");
+}
