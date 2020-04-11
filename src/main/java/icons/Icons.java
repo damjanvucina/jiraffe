09d87ca69661dac9952c6cea3965ecb6b5439327
Damjan Vucina <vucinadamjan@gmail.com>
2020-04-11 23:59:15 +0200

    Add settings option

--- src/main/java/icons/Icons.java
+++ src/main/java/icons/Icons.java
@@ -9,4 +9,5 @@ public class Icons {
     public static final Icon jiraffeLogo40px = IconLoader.getIcon("/icons/jiraffe-logo-40px.svg");
     public static final Icon clock = IconLoader.getIcon("/icons/clock.svg");
     public static final Icon gitBranch = IconLoader.getIcon("/icons/git-branch.svg");
+    public static final Icon settings = IconLoader.getIcon("/icons/settings.svg");
 }
