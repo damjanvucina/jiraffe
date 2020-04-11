ed5d4cc5847405773c5c673aabd3e2473fcde750
Damjan Vucina <vucinadamjan@gmail.com>
2020-04-11 13:41:12 +0200

    Add plugin icons

--- src/main/java/com/damjanvucina/jiraffe/icons/Icons.java
+++ src/main/java/com/damjanvucina/jiraffe/icons/Icons.java
@@ -5,5 +5,7 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class Icons {
-    public static final Icon jiraffeIcon = IconLoader.getIcon("/icons/jiraffe-logo.svg");
+    public static final Icon jiraffeLogo = IconLoader.getIcon("/icons/jiraffe-logo.svg");
+    public static final Icon clock = IconLoader.getIcon("/icons/clock.svg");
+    public static final Icon gitBranch = IconLoader.getIcon("/icons/git-branch.svg");
 }
